025d1afbdd06893532c7bc467eba6fb8a0edea8f
Lukas Elflein <lukaselflein@gmail.com>
2019-06-04 14:19:02 +0000

    working draft for one SDS without water

--- create_substrate.py
+++ create_substrate.py
@@ -7,6 +7,7 @@ Author: Lukas Elflein <elfleinl@cs.uni-freiburg.de>
 import ase.build
 import ase.io
 import pandas as pd
+import csv
 
 
 def build_substrate(size=(12,14,6), lattice_constant=4.075):
@@ -23,7 +24,7 @@ def build_substrate(size=(12,14,6), lattice_constant=4.075):
                              orthogonal=True)
     return struc
 
-def clean_pdb(struc):
+def old_clean_pdb(struc):
     ase.io.write('unclean.pdb', struc)
     df = pd.read_csv('unclean.pdb', sep=r'\s+', skiprows=2, skipfooter=1, 
                      header=None, engine='python')
@@ -33,13 +34,39 @@ def clean_pdb(struc):
 
     # Replace 'Mol' with 'SUR' (for "surface")
     df[3] = 'SURF'
-
-    df.to_csv('tidy.csv', sep='\t', header=False, index=False, float_format='%.3f')
-
-    with open('tidy.csv', 'a') as infile:
-        infile.write('TER\t{}\t \tSUR\t{}\n'.format(len(df), len(df)+1))
-        infile.write('END')
     
+    df[0] += ' '
+
+    df.to_csv('test.pdb', sep=' ', header=False, index=False, float_format='%2.3f',  quoting=csv.QUOTE_NONE, escapechar=' ')
+
+    newfile = []
+    with open('test.pdb', 'r') as infile:
+        for line in infile:
+            line = line.replace(' ', '  ')
+            newfile += line
+
+    with open('test.pdb', 'w') as outfile:
+        outfile.write(''.join(newfile))
+        outfile.write('TER       0         SUR\n')
+        outfile.write('END\n')
+    
+def clean_pdb(struc):
+    ase.io.write('unclean.pdb', struc)
+
+    formatted_lines = []
+    with open('unclean.pdb', 'r') as infile:
+        # Skip first two lines
+        infile.readline()
+        infile.readline()
+        i = 1
+        for line in infile:
+            line = line.replace('MOL     1', 'SURF {:4d}'.format(i))
+            formatted_lines += line
+            i += 1
+
+    with open('test.pdb', 'w') as outfile:
+        outfile.write(''.join(formatted_lines))
+
 
 def main():
     struc = build_substrate()
